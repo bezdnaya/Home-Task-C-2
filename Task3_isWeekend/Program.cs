2c5065aeef68c58a9b0c13f89b858b4a22df0004
Irina <bezdnaya@mail.ru>
2022-11-24 13:27:28 +0200

    Новая версия определителя выходного дня

--- Task3_isWeekend/Program.cs
+++ Task3_isWeekend/Program.cs
@@ -1,31 +1,8 @@
 ﻿Console.Write("Введите цифрой какой сейчас день недели ");
 int day = Convert.ToInt32(Console.ReadLine());
-if (day < 8)
-{
-switch (day)
-{
-    case 1:
-        Console.WriteLine("Понедельник. Ура на работу");
-        break;
-    case 2:
-       Console.WriteLine("Вторник. Рабочий день"); 
-       break;
-    case 3:
-        Console.WriteLine("Среда. Снова работа");
-        break;
-    case 4:
-        Console.WriteLine("Четверг. Ура, завтра пятница");
-        break;
-    case 5:
-        Console.WriteLine("Ура, последний рабочий день");
-        break;
-    case 6:
-        Console.WriteLine("Ура, отдыхаем");
-        break;
-    case 7:
-        Console.WriteLine("Ура, отдыхаем");
-        break;
-}
-}
-else
-    Console.WriteLine("Неправильно введён день недели");
+if (day < 6)
+Console.WriteLine("Это рабочий день");
+if (day > 5 && day < 8)
+Console.WriteLine("Это выходной");
+if (day > 7)
+Console.WriteLine("Такого дня недели не существует");
