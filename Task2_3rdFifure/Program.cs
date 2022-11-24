880a9ef3511f1cc3f03b4ece2ca6b351b25d67c8
Irina <bezdnaya@mail.ru>
2022-11-24 13:00:10 +0200

    Finding 3rd figure in number

---
+++ Task2_3rdFifure/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine ("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+    Console.WriteLine("Третьей цифры нет");
+if (number > 99 && number < 1000)
+    Console.WriteLine($"Третья цифра {number%10}");
+if (number > 999)
+{
+    string strNumber = number.ToString();
+    int n = strNumber.Length;
+    int index = 0;
+    int count = 1;
+    while (index < n-3)
+    {
+        count = count * 10;
+        index++;
+    }
+    number = number / count;
+    Console.WriteLine($"Третья цифра {number%10}");
+
+
+    
+
+}
